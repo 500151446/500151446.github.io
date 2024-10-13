20739e32bd4ad85cb31fcecdbb8bd67e8ca95759
Tristen Smith <500151446@fvtc.edu>
2024-10-12 21:21:49 -0500

    added carousel

--- site.js
+++ site.js
@@ -18,3 +18,48 @@ else if (isEvening) {
 welcomeMessageHeader.textContent = welcomeMessage
 
 localStorage.setItem("It's a secret to everybody.", 'I had to look it up. I was a wee lad when I played the first zelda game on my gameboy')
+
+const urls = [
+    'https://images.pexels.com/photos/1454360/pexels-photo-1454360.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    'https://images.pexels.com/photos/933964/pexels-photo-933964.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    'https://images.pexels.com/photos/267885/pexels-photo-267885.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    'https://images.pexels.com/photos/1251861/pexels-photo-1251861.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    'https://images.pexels.com/photos/1370296/pexels-photo-1370296.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+].map(url => { (new Image()).src = url; return url })
+
+const images = document.querySelectorAll('#carousel img')
+
+let currentImage = 0
+const showImages = () => {
+    const offset = currentImage % urls.length
+    images.forEach((image, index) => {
+        const imageIndex = (index + offset + urls.length) % urls.length
+        image.src = urls[imageIndex]
+    })
+}
+
+showImages()
+
+const prevButton = document.querySelector('#prev')
+const nextButton = document.querySelector('#next')
+
+prevButton.addEventListener('click', () => {
+    console.log('prev button clicked')
+    currentImage--
+    console.log(currentImage)
+    showImages()
+})
+
+nextButton.addEventListener('click', () => {
+    console.log('next button clicked')
+    currentImage++
+    console.log(currentImage)
+    showImages()
+})
+
+setInterval(() => {
+    currentImage++
+    console.log('next image')
+    showImages()
+}, 5000)
+
